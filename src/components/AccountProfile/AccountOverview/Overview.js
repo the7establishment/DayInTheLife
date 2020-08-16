bd0322b111fbc82e65b1668485b9abbb60b444e4
anarchy99 <tschell264@gmail.com>
2020-08-16 14:30:26 -0400

    Add Account Profile logic p2

--- src/components/AccountProfile/AccountOverview/Overview.js
+++ src/components/AccountProfile/AccountOverview/Overview.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React from "react"
+import User from "./User"
 
 export default function () {
     return (
-        <div className="overview1">
+        <div className="overview1 viewbox">
             <img className="overview1-pic" alt="" src={require('../../../resource/profile/profilePicMatthew.jpg')}></img>
             <p className="largertext">Matthew Chuong</p>
-            <p className="smalltext">Gay Pride Activist</p>
+            <p className="smalltext">Software Developer</p>
             <p className="xsmalltext">5 years</p>
             <hr/>
             <p className="smalltext">Non-binary Male, From Hotlanta</p>
--- src/components/AccountProfile/Stage/Stage.js
+++ src/components/AccountProfile/Stage/Stage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function () {
     return (
-        <div className="stage">
+        <div className="stage viewbox">
             <h2>Stage</h2>
         </div>
     )
