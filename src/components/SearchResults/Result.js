eff54f690b99e518a5114cd3aef1a4122c4aa5ed
Matthew Chuong <mstchuong@yahoo.com>
2020-08-19 19:17:40 -0400

    Make a Static Results Card
    -Add empty heart icon
    -Add heart icon

--- src/components/SearchResults/Result.js
+++ src/components/SearchResults/Result.js
@@ -1,23 +1,50 @@
 import React, { useState } from "react";
-import { getMenuOptions } from '../AccountProfile/HeaderMenu'
+import heart from '../../resource/icons/heart.png'
 
-export default function Result(props) {
+export default class Result extends React.Component {
 
-  return (
-    <div className="result-card">
+  setCurrentTab = (event) => {
+    var active = document.getElementById("tab-active")
+    if(event.target.innerText == "Tool"){
+      active.setAttribute("style", "grid-column:1; width: 33px")
+    }
+    if(event.target.innerText == "Qualifications"){
+      active.setAttribute("style", "grid-column:2; width: 95px")
+    }
+    if(event.target.innerText == "Listings"){
+      active.setAttribute("style", "grid-column:3; width: 54px")
+    }
+  }
+
+  render(){
+    return (
+      <div className="result-card">
         <div className="result-left">
-          
+          <div className="result-detail-left">
+            Left
+          </div>
+          <div className="result-detail-right">
+            <span className="result-title">Software Developer</span>
+            <span>United Healthgroup</span>
+            <span>Miami, FL</span>
+            <li className="result-list gray">Software Development: 3 Years (Preferred)</li>
+            <li className="result-list gray">Interface with our existing developers to help support issues and bugs.</li>
+            <li className="result-list gray">Build and maintain MVC and REST applications.</li>
+            <img src={heart} alt="" className="resulticon"></img>
+          </div>
         </div>
         <div className="result-right">
           <div className="result-tabmenu">
-            <span className="tab">Tool</span>
-            <span className="tab">Qualifications</span>
-            <div className="tab-active"></div>
+            <span className="tab" onClick={this.setCurrentTab}>Tool</span>
+            <span className="tab" onClick={this.setCurrentTab}>Qualifications</span>
+            <span className="tab" onClick={this.setCurrentTab}>Listings</span>
+            <div id="tab-active"></div>
           </div>
           <div className="result-tabbody">
             Body
           </div>
         </div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
