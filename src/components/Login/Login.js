d1b410d1a510429da550f17b53285f4c5e088bd4
Matthew Chuong <mstchuong@yahoo.com>
2020-09-06 19:53:02 -0400

    :white_check_mark: Validate Signup and Login
    -Add clearForm, verifyName, verifyEmail, verifyPass, verifyEmailNamePass, login
    -Add wrapper class to set height

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -10,7 +10,12 @@ export default class Login extends React.Component {
     super()
     this.state = {
       isLogin: true,
-      show: false
+      show: false,
+      passErrMsg: '',
+      emailErrMsg: '',
+      nameErrMsg: '',
+      hasError: false,
+      valid: true
     }
   }
 
@@ -27,6 +32,26 @@ export default class Login extends React.Component {
       this.setState({isLogin:false})
     else
       this.setState({isLogin:true})
+    this.clearForm()
+  }
+
+  clearForm = () => {
+    this.setState({
+      nameErrMsg: '',
+      emailErrMsg: '',
+      passErrMsg: '',
+      hasError:false,
+      valid:true
+    })
+    var email = document.getElementById("Email")
+    var name = document.getElementById("Name")
+    var pass = document.getElementById("Password")
+    email.classList.remove("error")
+    email.value = ""
+    pass.classList.remove("error")
+    pass.value = ""
+    name.classList.remove("error")
+    name.value = ""
   }
 
   showHidePassword = () => {
@@ -37,6 +62,85 @@ export default class Login extends React.Component {
       this.setState({show:true})
   }
 
+  verifyName = () => {
+    var name = document.getElementById("Name")
+    var re = /([a-zA-Z]).{3,20}/
+    if(name.value == 0){
+      name.classList.add("error")
+      this.setState({nameErrMsg: "Please enter a name.", hasError: true, valid: false})
+      return false
+    }
+    else if(!re.test(name.value)){
+      name.classList.add("error")
+      this.setState({nameErrMsg: "Please enter between 3 and 20 characters.", hasError: true, valid: false})
+      return false
+    }
+    else {
+      name.classList.remove("error")
+      this.setState({nameErrMsg: "",hasError: false, valid: true})
+      return true
+    }
+  }
+
+  verifyPassword = () => {
+    var pass = document.getElementById("Password")
+    var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}/
+    if(pass.value.length == 0){
+      pass.classList.add("error")
+      this.setState({passErrMsg: "Please enter a password.", hasError: true, valid: false})
+      return false
+    }
+    else if(!re.test(pass.value)){
+      pass.classList.add("error")
+      this.setState({passErrMsg: "Passwords must be 8 to 20 characters, contain more than 2 types of characters, and not include spaces.", hasError: true, valid: false})
+      return false
+    }
+    else {
+      pass.classList.remove("error")
+      this.setState({passErrMsg: "",hasError: false, valid: true})
+      return true
+    }
+  }
+
+  verifyEmail = () => {
+    var email = document.getElementById("Email")
+    var re = /.+\@[a-z]+\.[a-z]+/
+    if(email.value.length == 0){
+      email.classList.add("error")
+      this.setState({emailErrMsg: "Please enter a email address.", hasError: true, valid: false})
+      return false
+    }
+    else if(!re.test(email.value)) {
+      email.classList.add("error")
+      this.setState({emailErrMsg: "Please provide a valid email address.", hasError: true, valid: false})
+      return false
+    }
+    else {
+      email.classList.remove("error")
+      this.setState({emailErrMsg: "", hasError: false, valid: true})
+      return true
+    }
+  }
+
+  validateEmailNamePass = () => {
+    var validEmail = this.verifyEmail()
+    var validPass = this.verifyPassword()
+    if(validEmail && validPass)
+      return true
+    else
+      return false
+  }
+
+  login = () => {
+    var isValid = this.validateEmailNamePass()
+    if(this.state.isLogin){
+      if(isValid)
+        this.setState({valid:isValid})
+      else
+        this.setState({valid:isValid})
+    }
+  }
+
   render() {
     return (
       <div id="login" className="login modalbackground">
@@ -45,11 +149,18 @@ export default class Login extends React.Component {
             <span className="modaltitle">{this.state.isLogin ? 'LOG IN' : 'SIGN UP'}</span>
           </div>
           <div className="modalbody">
-            <input className={this.state.isLogin ? 'none' : 'modalinput'} placeholder="Name"/>
-            <input className="modalinput" placeholder="Email"/>
-            <div className="passwordwrap">
-              <input className="modalinput" placeholder="Password" type={this.state.show ? "text" : "password"}/>
+            <div className={this.state.isLogin ? 'none' : 'wrapper'}>
+              <input className="modalinput" id="Name" placeholder="Name" onBlur={this.verifyName} onChange={this.verifyName}/>
+              <p className="errMsg">{this.state.nameErrMsg}</p>
+            </div>
+            <div className="wrapper">
+              <input className="modalinput" id="Email" placeholder="Email" onBlur={this.verifyEmail} onChange={this.verifyEmail}/>
+              <p className="errMsg">{this.state.emailErrMsg}</p>
+            </div>
+            <div className="wrapper">
+              <input className="modalinput" id="Password" placeholder="Password" type={this.state.show ? "text" : "password"} size="20" onChange={this.verifyPassword} onBlur={this.verifyPassword}/>
               <img className="showicon" alt="eye" src={this.state.show ?slash_eye : eye} onClick={this.showHidePassword}></img>
+              <p className="errMsg">{this.state.passErrMsg}</p>
             </div>
             <div className={this.state.isLogin ? 'rememberforgotbar' : 'none'}>
               <div className="remembermebox">
@@ -60,7 +171,7 @@ export default class Login extends React.Component {
                 <span className="forgotpassword">Forgot Password?</span>
               </div>
             </div>
-            <button className="ditl-button modalbutton">{this.state.isLogin ? 'LOG IN' : 'SIGN UP'}</button>
+            <button className={this.state.valid ? "ditl-button modalbutton" : "ditl-button modalbutton button-disabled"} onClick={this.login}>{this.state.isLogin ? 'LOG IN' : 'SIGN UP'}</button>
             <div className="logintypebox">
               <span className="signtypehint">{this.state.isLogin ? 'No account yet?' : 'Already have an account?'}</span>
               <span className="signuplink" onClick={this.changeLoginType}>{this.state.isLogin ? 'Sign up' : 'Log in'}</span>
