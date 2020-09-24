8143d0af5c29e2ac12524dedd63485a9444a95f5
Matthew Chuong <mstchuong@yahoo.com>
2020-09-23 20:16:07 -0400

    Link Search Results to Account Profile

--- src/components/SearchResults/Card.js
+++ src/components/SearchResults/Card.js
@@ -4,6 +4,7 @@ import empty_heart from '../../resource/icons/empty_heart.png'
 import star from '../../resource/icons/star.png'
 import Heatmap from "./HeatMap";
 import HeaderMenu from "../AccountProfile/HeaderMenu";
+import { Link } from 'react-router-dom'
 
 export default class Card extends React.Component {
   constructor(props) {
@@ -39,7 +40,9 @@ export default class Card extends React.Component {
               <span className="star-number">{card.starNumber}</span>
             </span>
             <span className={card.new ? 'card-new' : 'none'}>new</span>
-            <button className="card-button">View</button>
+            <Link to="/AccountProfile">
+              <button className="card-button">View</button>
+            </Link>
           </div>
           <div className="card-detail-right">
             <span className="card-title">{card.title}</span>
