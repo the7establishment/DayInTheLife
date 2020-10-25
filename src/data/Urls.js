e4ae5709d1b04642694603cd08f957da3cbba902
Matthew Chuong <mstchuong@yahoo.com>
2020-10-25 12:48:47 -0400

    Take out hard coded colon in url

--- src/data/Urls.js
+++ src/data/Urls.js
@@ -1,10 +1,10 @@
 const protocol = process.env.REACT_APP_PROTOCOL || "http";
 const hostname = process.env.REACT_APP_HOSTNAME || "localhost";
-const port = process.env.PORT || "3500";
+const port = process.env.PORT || ":3500";
 
 export const RestUrls = {
-    user: `${protocol}://${hostname}:${port}/user`,
-    users: `${protocol}://${hostname}:${port}/users`,
-    days: `${protocol}://${hostname}:${port}/days`,
-    products: `${protocol}://${hostname}:${port}/products`,
+    user: `${protocol}://${hostname}${port}/user`,
+    users: `${protocol}://${hostname}${port}/users`,
+    days: `${protocol}://${hostname}${port}/days`,
+    products: `${protocol}://${hostname}${port}/products`,
 }
