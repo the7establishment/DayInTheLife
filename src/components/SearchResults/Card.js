0c83439dcf18ba736118f1c4f71ec8e3ae8096c7
Tram Nguyen <nguyent62525@sacredheart.edu>
2020-10-31 13:14:04 -0400

    Card clean up

--- src/components/SearchResults/Card.js
+++ src/components/SearchResults/Card.js
@@ -8,27 +8,27 @@ import { Link } from 'react-router-dom'
 
 export default class Card extends React.Component {
   constructor(props) {
-    super(props) 
+    super(props)
     this.state = {
-        currentTab: 0,
-        favorite: this.props.card.favorite
+      currentTab: 0,
+      favorite: this.props.card.favorite
     }
   }
 
   setFavorite = () => {
-    if(this.state.favorite)
-      this.setState({favorite: false})
+    if (this.state.favorite)
+      this.setState({ favorite: false })
     else
-      this.setState({favorite: true})
+      this.setState({ favorite: true })
   }
 
   render() {
     var { card } = this.props
     var details = card.details.map((detail) => <li key={detail} className="card-list gray">{detail}</li>)
     var stars = []
-    for(var i = 0; i < parseInt(card.starNumber); i++){
+    /* for (var i = 0; i < parseInt(card.starNumber); i++) {
       stars.push(<img key={i} src={star} alt="" className="star"></img>)
-    }
+    } */
     return (
       <div className="card">
         <div className="card-left">
@@ -36,9 +36,9 @@ export default class Card extends React.Component {
             <img src={card.image} alt="" className="cardprofile"></img>
             <span className="stars-box">
               {stars}
-              <span className="star-number">{card.starNumber}</span>
+              {/* <span className="star-number">{card.starNumber}</span> */}
             </span>
-            <span className={card.new ? 'card-new' : 'none'}>new</span>
+            {/* {<span className={card.new ? 'card-new' : 'none'}>new</span>} */}
             <Link to={card.type === 'Worker' ? "/AccountProfile" : "/JobProfile"}>
               <button className="card-button">View</button>
             </Link>
@@ -48,16 +48,16 @@ export default class Card extends React.Component {
             <span className="card-name">{card.name}</span>
             <span className="card-location">{card.location}</span>
             {details}
-            <img src={this.state.favorite ? heart : empty_heart} alt="" className="cardicon" onClick={this.setFavorite}></img>
+            {/* <img src={this.state.favorite ? heart : empty_heart} alt="" className="cardicon" onClick={this.setFavorite}></img> */}
           </div>
         </div>
         <div className="card-right">
           <div className="card-tabmenu">
-            <HeaderMenu 
-            header={card.cardRight.header} 
-            items={card.cardRight.tabs.map(tab => tab.name)} 
-            currentTab={ this.state.currentTab }
-                        callback={(newTab) => this.setState({currentTab: newTab})}/>
+            <HeaderMenu
+              header={card.cardRight.header}
+              items={card.cardRight.tabs.map(tab => tab.name)}
+              currentTab={this.state.currentTab}
+              callback={(newTab) => this.setState({ currentTab: newTab })} />
           </div>
           <div className="card-tabbody">
             {this.getCardTabContent()}
@@ -70,29 +70,31 @@ export default class Card extends React.Component {
   getCardTabContent = () => {
     var { currentTab } = this.state
     var { tabs } = this.props.card.cardRight
-    if(tabs[currentTab].name === 'Video'){
-      return (
-        <div id="card-video" className="card-tabcontent">
-          <iframe id="video" width="100%" height="100%" src={tabs[currentTab].content} allowFullScreen></iframe>
-        </div>
-      )
-    }
-    else if(tabs[currentTab].name === 'Popular Day'){
+        /* if(tabs[currentTab].name === 'Video'){
+          return (
+            <div id="card-video" className="card-tabcontent">
+              <iframe id="video" width="100%" height="100%" src={tabs[currentTab].content} allowFullScreen></iframe>
+            </div>
+          )
+        }  */
+    if (tabs[currentTab].name === 'Popular Day') {
       return (
         <div id="card-intro" className="card-tabcontent">
           <span>{tabs[currentTab].content}</span>
           <span>...</span>
+          <Link to="/JobProfile">
           <a>Read More</a>
+          </Link>
         </div>
       )
     }
-    else if(tabs[currentTab].name === 'Activity'){
-      return (
-        <div id="card-heatmap" className="card-tabcontent">
-          <Heatmap heatmap={tabs[currentTab].content}/>
-        </div>
-      )
-    }
+        /* else if(tabs[currentTab].name === 'Activity'){
+          return (
+            <div id="card-heatmap" className="card-tabcontent">
+              <Heatmap heatmap={tabs[currentTab].content}/>
+            </div>
+          )
+        } */
     else
       return <div>Not Found</div>
   }
