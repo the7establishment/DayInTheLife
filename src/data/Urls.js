d9d7d8adcf923330d8918a7e1469901c110f40f6
Matthew Chuong <mstchuong@yahoo.com>
2020-11-20 21:54:20 -0500

    no need to seperate string

--- src/data/Urls.js
+++ src/data/Urls.js
@@ -1,5 +1,5 @@
 const protocol = process.env.REACT_APP_PROTOCOL || "http";
-const hostname = process.env.REACT_APP_HOSTNAME || "localhost" + ":3500";
+const hostname = process.env.REACT_APP_HOSTNAME || "localhost:3500";
 
 export const Urls = {
     user: `${protocol}://${hostname}/user`,
