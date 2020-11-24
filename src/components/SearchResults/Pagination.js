79d548f2b62af3f85f35b72fb2fd98e8a8947913
Matthew Chuong <mstchuong@yahoo.com>
2020-11-23 21:37:53 -0500

    Add 5 new state variables
    -limit card items to 10
    -dynamically create page tabs at the bottom
    -highlight current page tab

--- src/components/SearchResults/Pagination.js
+++ src/components/SearchResults/Pagination.js
@@ -2,24 +2,27 @@ import React from "react";
 import leftarrow from '../../resource/icons/left-arrow.png'
 
 export default class Pagination extends React.Component {
-  constructor(props){
-    super()
-    this.state = {
-      pageCount: 0
+
+  showPage = () => {
+    console.log('works')
+  }
+  getPages = () => {
+    var pages=[]
+    for(var i = 1; i <= this.props.pageNum; i++){
+      pages.push(i)
     }
+    return pages.map(page => {
+      return <div className={this.props.pageCurrent===page ? "pagecurrent pagenum" : "pagenum"} onClick={this.showPage}>{page}</div>
+    })
   }
 
   render() {
     return (
       <div className="pagination-row">
         <div className="pages">
-          <img className="icon left-arrow" alt="" src={leftarrow}></img>
-          <div className="pagenum">1</div>
-          <div className="pagenum">2</div>
-          <div className="pagenum">3</div>
-          <div className="pagenum">4</div>
-          <div className="pagenum">5</div>
-          <img className="icon right-arrow" alt="" src={leftarrow}></img>
+          <img className="icon left-arrow" alt="" src={leftarrow} style={{height: '30px'}}></img>
+          {this.getPages()}
+          <img className="icon right-arrow" alt="" src={leftarrow} style={{height: '30px'}}></img>
         </div>
       </div>
     )
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -2,70 +2,78 @@ import React from "react"
 import '../../css/SearchResults/SearchResults.css'
 import Card from "./Card"
 import Pagination from "./Pagination"
-import { MockCards } from '../../mock/MockData'
 import work from '../../resource/icons/work.png'
 
 export default class SearchResults extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-      cards: MockCards,
       job: props.job,
       jobCards: [],
-      query: ""
-    } 
+      query: "",
+      pageCurrent: 1,
+      pageItems: 10,
+      pageList: [],
+      pageNum: 0
+    }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     var jobCards = []
-    if(this.state.job && typeof this.state.job === "object"){
+    if (this.state.job && typeof this.state.job === "object") {
       this.state.job.OccupationList.map(job => {
         jobCards.push(
-        {
-          type: 'Job',
-          jobId: job.OnetCode,
-          image: work,
-          title: job.OnetTitle,
-          name: '',
-          location: '',
-          starNumber: '5.0',
-          new: false,
-          favorite: false,
-          details: [],
-          cardRight: {
-            header: "",
-            tabs:[
-              {
-                name: 'Popular Day',
-                content: job.OccupationDescription
-              },
-            ]
-          }
-        })
+          {
+            type: 'Job',
+            jobId: job.OnetCode,
+            image: work,
+            title: job.OnetTitle,
+            name: '',
+            location: '',
+            starNumber: '5.0',
+            new: false,
+            favorite: false,
+            details: [job.OccupationDescription],
+            cardRight: {
+              header: "",
+              tabs: [
+                {
+                  name: 'Popular Day',
+                  content: ""
+                },
+              ]
+            }
+          })
       })
+      this.setState({ jobCards: jobCards, pageNum: Math.ceil(jobCards.length/this.state.pageItems) })
     }
-    else{
-        this.setState({query: this.state.job})
+    else {
+      this.setState({ query: this.state.job })
     }
-      this.setState({jobCards: jobCards})
   }
 
+
   getCardList = () => {
-    return this.state.jobCards.map(card => {
-        return <Card key={card.jobId} card={card} />
+    var begin = (this.state.pageCurrent - 1) * this.state.pageItems
+    var end = begin + this.state.pageItems
+    var pageList = []
+    pageList = this.state.jobCards.slice(begin, end)
+    return pageList.map(card => {
+      return <Card key={card.jobId} card={card} />
     })
   }
 
-  render(){
+  render() {
     var query = window.location.search
     return (
       <div className="searchresults gray">
-      <div className="results-column">
-        <label className={this.state.jobCards.length > 0 ? "results-count" : "none"}>{this.state.jobCards.length} Results Found for {this.state.job.Request.InputOccupation}</label>
-    <h1 className={typeof this.state.job === "object" ? "none" : "" }>No Results for {this.state.query}</h1>  
-        {this.getCardList()}
-        <Pagination />
+        <div className="results-column">
+          <label className={this.state.jobCards.length > 0 ? "results-count" : "none"}>{this.state.jobCards.length} Results Found for {this.state.job.Request.InputOccupation}</label>
+          <h1 className={typeof this.state.job === "object" ? "none" : ""}>No Results for {this.state.query}</h1>
+          {this.getCardList()}
+          <Pagination pageNum={this.state.pageNum} pageCurrent={this.state.pageCurrent}/>
+        </div>
       </div>
-    </div>
-  )}
+    )
+  }
 }
