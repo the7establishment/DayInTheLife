39c6acca6b61ce6d9360d574f7d66b107526cf5f
anarchy99 <tschell264@gmail.com>
2021-02-25 22:48:20 -0500

    Maintain state between login signup

--- src/components/LoginSignup/LoginSignup.js
+++ src/components/LoginSignup/LoginSignup.js
@@ -10,6 +10,7 @@ const EMPTY_NAME_MESSAGE = 'Please enter a name.';
 const INVALID_NAME_MESSAGE = 'Please enter at least 3 characters.';
 const EMPTY_PASSWORD_MESSAGE = 'Please enter a password.';
 const INVALID_PASSWORD_MESSAGE = 'Passwords must be 8 to 20 characters, contain at least 1 uppercase and lowercase letter, 1 number, and not include spaces.';
+const SERVICE_DOWN_MESSAGE = 'Service is not available at this time. Please try again later.'
 
 var dataSource = new RestDataSource()
 
@@ -19,19 +20,35 @@ export default class LoginSignup extends React.Component {
     this.state={
       currentForm: "Login",
       show: false,
-      serviceErrMsg:'',
-      passErrMsg: '',
-      emailErrMsg: '',
-      nameErrMsg: '',
-      hasError: false,
-      validName: false,
-      validEmail: false,
-      validPwd: false,
-      valid: false,
+      // error messages
+      loginErrMsg:'',
+      signUpErrMsg:'',
+      loginPassErrMsg: '',
+      loginEmailErrMsg: '',
+      signupPassErrMsg: '',
+      signupEmailErrMsg: '',
+      firstNameErrMsg: '',
+      lastNameErrMsg: '',
+      // login
+      loginEmail: '',
+      loginPassword: '',      
+      loginHasError: false,
+      firstValidName: false,
+      lastValidName: false,
+      loginValidEmail: false,
+      loginValidPwd: false,
+      loginValid: false,
+      // signup
+      firstValidName: false,
+      lastValidName: false,
+      signupHasError: false,
+      signupValidEmail: false,
+      signupValidPwd: false,
+      signupValid: false,
       firstName: '',
       lastName:'',
-      email: '',
-      password: '',
+      signupEmail: '',
+      signupPassword: '',
       gender: 'Other',
       country: 'United States',
       region: '',
@@ -43,13 +60,12 @@ export default class LoginSignup extends React.Component {
   changeLoginType = () => {
     var isLogin = this.state.currentForm === "Login"
     if (isLogin)
-      this.setState({ currentForm: "Form1", validName : false, validEmail : false, validPwd: false })
+      this.setState({ currentForm: "Form1" })
     else
-      this.setState({ currentForm: "Login", validName : false, validEmail : false, validPwd : false })
-    this.clearForm()
+      this.setState({ currentForm: "Login" })
   }
 
-  clearForm = () => {
+  clearForm = (mode) => {
     this.setState({
       nameErrMsg: '',
       emailErrMsg: '',
@@ -74,11 +90,11 @@ export default class LoginSignup extends React.Component {
   checkForm = () => {
     var isLogin = this.state.currentForm === "Login"
     if(isLogin) {
-      if(this.state.validEmail && this.state.validPwd) 
-        this.setState({ hasError: false, valid: true })
+      if(this.state.loginValidEmail && this.state.loginValidPwd) 
+        this.setState({ loginHasError: false, loginValid: true })
     } else {
-      if(this.state.validName && this.state.validEmail && this.state.validPwd) 
-        this.setState({ hasError: false, valid: true })
+      if(this.state.firstValidName && this.state.lastValidName && this.state.signupValidEmail && this.state.signupValidPwd) 
+        this.setState({ signupHasError: false, signupValid: true })
     }
   }
 
@@ -94,62 +110,75 @@ export default class LoginSignup extends React.Component {
       this.setState({ show: true })
   }
 
-  validateName = (arg) => {
-    var name = arg.target ? arg.target : document.getElementById(arg)
+  validateName = (type) => {
+    var elementId = type + "Name" 
+    var validName = type + "ValidName"
+    var nameErrMsg = type + "NameErrMsg"
+    var name = document.getElementById(elementId)
     var re = /([a-zA-Z]).{3,50}/
     if (name.value.length === 0) {
       name.classList.add("error")
-      this.setState({ nameErrMsg: EMPTY_NAME_MESSAGE, hasError: true, valid: false })
+      this.setState({ [nameErrMsg]: EMPTY_NAME_MESSAGE, signupHasError: true, signupValid: false, [name.id]: name.value })
       return false
     }
     else if (!re.test(name.value)) {
       name.classList.add("error")
-      this.setState({ nameErrMsg: INVALID_NAME_MESSAGE, hasError: true, valid: false })
+      this.setState({ [nameErrMsg]: INVALID_NAME_MESSAGE, signupHasError: true, signupValid: false, [name.id]: name.value })
       return false
     }
     else {
       name.classList.remove("error")
-      this.setState({ nameErrMsg: "", validName : true, [name.id]: name.value}, this.checkForm)
+      this.setState({ [nameErrMsg]: "", [validName] : true, [name.id]: name.value}, this.checkForm)
       return true
     }
   }
 
-  validatePwd = () => {
-    var pass = document.getElementById("Password")
+  validatePwd = (mode) => {
+    var elementId = mode + "Password"
+    var valid = mode + "Valid"
+    var hasError = mode + "HasError"
+    var validPwd = mode + "ValidPwd"
+    var passErrMsg = mode + "PassErrMsg"
+    var pass = document.getElementById(elementId)
     var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\S{8,20}$/
     if (pass.value.length === 0) {
       pass.classList.add("error")
-      this.setState({ passErrMsg: EMPTY_PASSWORD_MESSAGE, hasError: true, valid: false })
+      this.setState({ [passErrMsg]: EMPTY_PASSWORD_MESSAGE, [hasError]: true, [valid]: false, [pass.id]: pass.value })
       return false
     }
     else if (!re.test(pass.value)) {
       pass.classList.add("error")
-      this.setState({ passErrMsg: INVALID_PASSWORD_MESSAGE, hasError: true, valid: false })
+      this.setState({ [passErrMsg]: INVALID_PASSWORD_MESSAGE, [hasError]: true, [valid]: false, [pass.id]: pass.value })
       return false
     }
     else {
       pass.classList.remove("error")
-      this.setState({ passErrMsg: "", validPwd : true, password: pass.value }, this.checkForm)
+      this.setState({ [passErrMsg]: "", [validPwd] : true, [pass.id]: pass.value }, this.checkForm)
       return true
     }
   }
 
-  validateEmail = () => {
-    var email = document.getElementById("Email")
+  validateEmail = (mode) => {
+    var elementId = mode + "Email"
+    var valid = mode + "Valid"
+    var hasError = mode + "HasError"
+    var validEmail = mode + "ValidEmail"
+    var emailErrMsg = mode + "EmailErrMsg"
+    var email = document.getElementById(elementId)
     var re = /.+@[a-z]+\.[a-z]+/
     if (email.value.length === 0) {
       email.classList.add("error")
-      this.setState({ emailErrMsg: "Please enter a email address.", hasError: true, valid: false })
+      this.setState({ [emailErrMsg]: "Please enter an email address.", [hasError]: true, [valid]: false, [email.id]: email.value })
       return false
     }
     else if (!re.test(email.value)) {
       email.classList.add("error")
-      this.setState({ emailErrMsg: "Please provide a valid email address.", hasError: true, valid: false })
+      this.setState({ [emailErrMsg]: "Please provide a valid email address.", [hasError]: true, [valid]: false, [email.id]: email.value })
       return false
     }
     else {
       email.classList.remove("error")
-      this.setState({ emailErrMsg: "", validEmail : true, email: email.value }, this.checkForm)
+      this.setState({ [emailErrMsg]: "", [validEmail] : true, [email.id]: email.value }, this.checkForm)
       return true
     }
   }
@@ -195,15 +224,15 @@ export default class LoginSignup extends React.Component {
   }
 
   validateLogin() {
-    var validEmail = this.validateEmail();
-    var validPwd = this.validatePwd();
+    var validEmail = this.validateEmail('login');
+    var validPwd = this.validatePwd('login');
     return validEmail && validPwd;
   }
 
   async executeLogin() {
     var { openOrCloseLoginModal } = this.props
-    var email = this.state.email
-    var password = this.state.password
+    var email = this.state.loginEmail
+    var password = this.state.loginPassword
 
 
     // post login
@@ -212,7 +241,8 @@ export default class LoginSignup extends React.Component {
         openOrCloseLoginModal();
         window.location.href = `/Home?user=${res.data.userId}`
     }).catch(error => {
-      this.setState({ serviceErrMsg: error.response.data.message })
+      var errorMessage = error.response ? error.response.data.message : SERVICE_DOWN_MESSAGE
+      this.setState({ loginErrMsg: errorMessage })
       console.error('Login post call failed: ' + error.message);
     })
   }
@@ -223,8 +253,8 @@ export default class LoginSignup extends React.Component {
       firstName: this.state.firstName,
       lastName: this.state.lastName,
       fullName: this.state.firstName + " " + this.state.lastName,
-      email: this.state.email,
-      password: this.state.password,
+      email: this.state.signupEmail,
+      password: this.state.signupPassword,
       gender: this.state.gender,
       country: this.state.country,
       region: this.state.region,
@@ -239,7 +269,8 @@ export default class LoginSignup extends React.Component {
         openOrCloseLoginModal();
         window.location.href = `/Home?user=${res.data.userId}`
     }).catch(error => {
-      this.setState({ serviceErrMsg: error.response.data.message })
+      var errorMessage = error.response ? error.response.data.message : SERVICE_DOWN_MESSAGE
+      this.setState({ signupErrMsg: errorMessage })
       console.error('Signup post call failed: ' + error.message);
     })
   }
@@ -254,11 +285,13 @@ export default class LoginSignup extends React.Component {
             validatePwd:this.validatePwd,
             showHidePassword:this.showHidePassword,
             submit:this.submit,
-            serviceErrMsg:this.state.serviceErrMsg,
-            emailErrMsg:this.state.emailErrMsg,
-            passErrMsg:this.state.passErrMsg,
-            valid:this.state.valid,
+            serviceErrMsg:this.state.loginErrMsg,
+            emailErrMsg:this.state.loginEmailErrMsg,
+            passErrMsg:this.state.loginPassErrMsg,
+            valid:this.state.loginValid,
             show:this.state.show,
+            email:this.state.loginEmail,
+            password:this.state.loginPassword
           }
           return wrap(Login, args)
         case "Form1" :
@@ -269,12 +302,17 @@ export default class LoginSignup extends React.Component {
             showHidePassword:this.showHidePassword,
             changeForm:this.changeForm,
             next: this.nextForm,
-            serviceErrMsg:this.state.serviceErrMsg,
-            nameErrMsg:this.state.nameErrMsg, 
-            emailErrMsg:this.state.emailErrMsg, 
-            passErrMsg:this.state.passErrMsg,
+            serviceErrMsg:this.state.signupErrMsg,
+            firstNameErrMsg:this.state.firstNameErrMsg, 
+            lastNameErrMsg:this.state.lastNameErrMsg, 
+            emailErrMsg:this.state.signupEmailErrMsg, 
+            passErrMsg:this.state.signupPassErrMsg,
             show:this.state.show,
-            valid:this.state.valid
+            valid:this.state.signupValid,
+            firstName:this.state.firstName,
+            lastName:this.state.lastName,
+            email:this.state.signupEmail,
+            password:this.state.signupPassword
           }
           return wrap(Form1, args)
         case "Form2" :
@@ -286,13 +324,13 @@ export default class LoginSignup extends React.Component {
             updateHomeCountry:this.updateHomeCountry,
             updateRegion:this.updateRegion,
             updateHomeRegion:this.updateHomeRegion,
-            serviceErrMsg:this.state.serviceErrMsg,
+            serviceErrMsg:this.state.signupErrMsg,
             gender:this.state.gender,
             country:this.state.country,
             region:this.state.region,
             homeCountry:this.state.homeCountry,
             homeRegion:this.state.homeRegion,
-            valid:this.state.valid
+            valid:this.state.signupValid
           }
           return wrap(Form2, args)
       }
